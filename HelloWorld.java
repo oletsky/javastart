bfc9ac1ebd5c1a6ab57d6dbb491d2f71f8a64c85
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-18 20:27:23 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package javatraining.oletsky.javastart;
+
+/**
+ * @author O.Oletsky
+ * Canonical Hello, world
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java world!");
+    }
+}
