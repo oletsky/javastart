5b75363c6383f4d77738047f1e8da67d950e2aca
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-18 21:08:56 +0300

    Add files via upload

---
+++ CalculateExponent.java
@@ -0,0 +1,14 @@
+package javatraining.oletsky.javastart;
+
+/**
+ * @author O.Oletsky
+ * Using ready classes - calculation of an exponent
+ */
+public class CalculateExponent {
+    public static void main(String[] args) {
+        double x = 1.;
+        double r=Math.exp(x);
+        System.out.println(r);
+
+    }
+}
