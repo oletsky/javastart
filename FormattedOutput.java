501a67ab0ccbf505b85ae406573b9aeaed381d95
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-18 20:47:55 +0300

    Add files via upload

---
+++ FormattedOutput.java
@@ -0,0 +1,13 @@
+package javatraining.oletsky.javastart;
+
+/**
+ * @author O.Oletsky
+ * Formatted output
+ */
+public class FormattedOutput {
+    public static void main(String[] args) {
+        double pi=Math.PI;
+        System.out.printf("PI is %8.4f",pi);
+    }
+
+}
